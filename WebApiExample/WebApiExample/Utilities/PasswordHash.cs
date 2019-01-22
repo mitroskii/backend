098188978e6ec9bf529e9388fd0dec386653a1b7
mitroskii <mitro.onatsu@hotmail.com>
2019-01-22 11:13:45 +0200

    Created Utilities + passwordhash

---
+++ WebApiExample/WebApiExample/Utilities/PasswordHash.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiExample.Utilities
+{
+    public class PasswordHash
+    {
+        /// <summary>
+        /// Hash password with salt.
+        /// Return hashed password as string.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="salt"></param>
+        /// <returns></returns>
+        public static string HashPassword(string password, string salt)
+        {
+            byte[] saltBytes = Encoding.ASCII.GetBytes(salt);
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: saltBytes,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+    }
+}
