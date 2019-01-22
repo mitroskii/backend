b57ef45633c23d9b9d0eece5e2948a1787e05ccf
mitroskii <mitro.onatsu@hotmail.com>
2019-01-22 10:10:45 +0200

    Created Services

---
+++ WebApiExample/WebApiExample/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiExample.Models;
+
+namespace WebApiExample.Services
+{
+    public interface IPersonService
+    {
+        //CRUD
+        Person Create(Person person);
+        List<Person> Read();
+        Person Read(int id);
+        Person Update(int id, Person person);
+        void Delete(int id);
+    }
+}
---
+++ WebApiExample/WebApiExample/Services/PersonService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiExample.Models;
+using WebApiExample.Repositories;
+
+namespace WebApiExample.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly IPersonRepository _personRepository;
+       
+
+        public PersonService(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        public Person Create(Person person)
+        {
+            return _personRepository.Create(person);
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> Read()
+        {
+            return _personRepository.Read();
+        }
+
+        public Person Read(int id)
+        {
+            return _personRepository.Read(id);
+        }
+
+        public Person Update(int id, Person person)
+        {
+            var savedPerson = _personRepository.Read(id);
+            if (savedPerson == null)   
+             throw new Exception("Person not found");
+            return _personRepository.Update(person);
+        }
+    }
+}
